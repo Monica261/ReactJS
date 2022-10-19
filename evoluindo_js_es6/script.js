cb45b6ae8d080631b2744a78e1dad07fa9f456c8
mnstorres <mnstorres@santacasasaudesjc.com.br>
2022-10-19 17:51:58 -0300

    buscador de cep usando api

---
+++ evoluindo_js_es6/script.js
@@ -0,0 +1,70 @@
+//condicional idade
+var idade = prompt('Sua idade?');
+if(idade >= 18){
+    alert(`${idade} - maior de idade`);
+}else{
+    alert(`${idade} - menor de idade`);
+};
+
+document.write(`${idade} - Idade do candidato`);
+
+//funcoes
+function entrar(){
+    var area = document.getElementById('area');
+    var texto = prompt('digite seu nome');
+
+    if(texto == '' || texto == null){
+    alert('Digite seu nome novamente');
+    area.innerHTML = 'Bem vindo...';
+    }else{
+    area.innerHTML =(`${texto} seja bem vindo`);
+    }
+}
+
+function entrada(idade, nome){
+    var call = document.getElementById('retorno');
+    var idade = prompt('digite sua idade');
+
+    call.innerHTML = idade + ' ' + nome;
+}
+
+function cor(parametroCor){
+    window.document.body.style.backgroundColor = parametroCor;
+}
+cor('teal');
+
+//cor de fundo escolha do usuario
+function cor(){
+    const parametroCor = prompt('cor do background em ingles?');
+    window.document.body.style.backgroundColor = parametroCor;
+}
+
+//array
+var lista = ['melão', 'maça', 'manga', 'banana', 'uva'];
+console.log(lista.length); //quantidade de itens na lista
+console.log(lista[1]); //retorno somente o item que desejo
+lista.concat('pera'); //incrementar a lista
+lista.indexOf('manga'); //encontrar a posição do item no array
+console.log(lista.join('/')); //juntar a lista com algo
+lista.pop; //remove o ultimo item
+lista.shift; //remove o primeiro
+
+if(lista.indexOf('maça') > 1){
+    alert('este item está na lista');
+}else{
+    alert('este item não está na lista');
+}
+
+function concatenarLista(){
+    const juncao = prompt('digite um item para incrementar a lista');
+
+    if(juncao == '' || juncao == null){ 
+        alert(lista.concat(juncao).innerHTML);
+    }else if(juncao != null){  
+        alert(lista + ',' + juncao);
+    }
+    else{
+        alert(lista).innerHTML;
+    }
+}
+concatenarLista();
