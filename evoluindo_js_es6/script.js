a4d7217938591c51706389c3b831a821a7857af7
mnstorres <mnstorres@santacasasaudesjc.com.br>
2022-11-16 10:52:00 -0300

    rest, spread, every, some, includes, startsWith e endsWith foram vistas em aulas

--- evoluindo_js_es6/script.js
+++ evoluindo_js_es6/script.js
@@ -153,6 +153,8 @@ console.log(pedidoComida({
     sobremesa: '1 casquinha de chocolate'
 }))
 
+/*-----------------------------*/
+
 //rest operator
 
 function cadastrar(usuarios, ...novosUsuarios){
@@ -168,3 +170,84 @@ function cadastrar(usuarios, ...novosUsuarios){
 let usuarios = ['Matheus', 'João'];
 
 let novosUsuarios = cadastrar(usuarios, 'Pablo', 'Maria', 'Agatha');
+
+/*-----------------------------*/
+
+//operação em array
+//map - percorre todos os itens e retorna algo
+const lista = [5, 5, 2, 4, 2, 1];
+
+const novaLista = lista.map(function(item){
+    return item * 5;
+});
+console.log(novaLista)
+
+//reduce - soma os itens da lista
+const soma = lista.reduce(function(total, proximo){
+    return total + proximo;
+})
+console.log(soma)
+
+//achar item atraves do find
+const procurarLista = lista.find(function(item){
+    return item === 4
+});
+console.log(procurarLista);
+
+/*-----------------------------*/
+
+//funções anonimas - quando eu declaro uma função dentro de outra de forma natural
+function adicionar(...numeros){
+    let total = numeros.reduce((total, proximo)=>{
+        let soma = total + proximo;
+        return soma;
+    })
+    console.log(total)
+}
+
+console.log(adicionar(1,2,3,4,5));
+
+/*-----------------------------*/
+
+//includes
+//endWith
+//startsWith
+
+//includes pode servir para verificação
+let colegas = ['monica', 'guilherme', 'thiago', 'fabio', 'gustavo', 'bahij'];
+
+let func = prompt('nome do func?');
+
+if(colegas.includes(func)){
+    document.write(`</br></br>${func} trabalha conosco </br></br>`)
+}else{
+    document.write(`</br></br>${func} NÃO trabalha conosco</br></br>`)
+}
+
+//startsWith verifica se a string esta começando com oq estou passando
+let palavra = 'planeta';
+console.log(palavra.startsWith('p')); //me retorna true ou false
+
+//endWith verifica se a string esta terminando com oq estou passando
+let frase = 'the life snake';
+console.log(frase.endsWith('e'));
+
+/*-----------------------------*/
+
+//some - funciona como um boolean
+let funcionario = ['pedro', 'carlos', 'antonio'];
+
+console.log(funcionario.some(nome => nome === 'pietro'));
+
+//every - todos precisam passar na condição
+let estudantes = [
+    {nome: 'Caio', idade: 20},
+    {nome: 'Antonio', idade: 15},
+    {nome: 'Maria', idade: 19}
+]
+
+if(estudantes.every(estudantes => estudantes.idade >= 18)){
+    console.log('todos sao maiores de idade')
+}else{
+    console.log('alguem é de menor')
+}
